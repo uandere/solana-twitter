1371a6399e69b616439a21fe070b09bebd1a33e5
Nazar Demchuk <nazar.demchuk@lynxtrading.com>
2024-07-28 22:27:29 +0300

    Episode 3 finished

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -1,3 +1,5 @@
+use std::mem::size_of;
+
 use anchor_lang::prelude::*;
 
 declare_id!("B3W8FdURNLEEJncJNEFfu3Jfui7PTbpAcuXDnbA4Ctin");
@@ -14,3 +16,26 @@ pub mod solana_twitter {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[account]
+pub struct Tweet {
+    pub author: Pubkey,
+    pub timestamp: i64,
+    pub topic: String,
+    pub content: String,
+}
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = size_of::<Pubkey>();
+const TIMESTAMP_LENGTH: usize = size_of::<i64>();
+const STRING_LENGTH_PREFIX: usize = 4;
+const MAX_TOPIC_LENGTH: usize = 50 * STRING_LENGTH_PREFIX;
+const MAX_CONTENT_LENGTH: usize = 280 * 4;
+
+impl Tweet {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH // Author.
+        + TIMESTAMP_LENGTH // Timestamp.
+        + STRING_LENGTH_PREFIX + MAX_TOPIC_LENGTH // Topic.
+        + STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH; // Content.
+}
