85d26687288aae8e6017b0118b8816cc96a4f4f5
Nazar Demchuk <nazar.demchuk@lynxtrading.com>
2024-07-29 11:38:02 +0300

    Finished episode_8

---
+++ app/src/composables/useWorkspace.js
@@ -0,0 +1,24 @@
+import { computed } from 'vue'
+import { useAnchorWallet } from 'solana-wallets-vue'
+import { Connection, PublicKey } from '@solana/web3.js'
+import { AnchorProvider, Program } from '@coral-xyz/anchor'
+import idl from '../../../target/idl/solana_twitter.json'
+
+const programID = new PublicKey(idl.address)
+let workspace = null
+
+export const useWorkspace = () => workspace
+
+export const initWorkspace = () => {
+    const wallet = useAnchorWallet()
+    const connection = new Connection('http://127.0.0.1:8899')
+    const provider = computed(() => new AnchorProvider(connection, wallet.value))
+    const program = computed(() => new Program(idl, programID, provider.value))
+
+    workspace = {
+        wallet,
+        connection,
+        provider,
+        program,
+    }
+}
