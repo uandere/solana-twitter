d272c5745cdf61cf1426b1a7b3a7fcd01f5b0545
Nazar Demchuk <nazar.demchuk@lynxtrading.com>
2024-07-29 10:25:44 +0300

    Added polyfills; Installed neccessary libs

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,4 +1,28 @@
+const webpack = require('webpack')
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    configureWebpack: {
+        plugins: [
+            new webpack.ProvidePlugin({
+                Buffer: ['buffer', 'Buffer']
+            })
+        ],
+        resolve: {
+            fallback: {
+                crypto: false,
+                fs: false,
+                assert: false,
+                process: false,
+                util: false,
+                path: false,
+                stream: require.resolve('stream-browserify'),
+                http: require.resolve('stream-http'),
+                https: require.resolve('https-browserify'),
+                zlib: require.resolve('browserify-zlib'),
+                url: require.resolve('url/'),
+            }
+        }
+    }
 })
