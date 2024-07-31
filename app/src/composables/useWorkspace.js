d61adf170f3487ca13841ea808c3ee5503fbfdc4
Nazar Demchuk <nazar.demchuk@lynxtrading.com>
2024-08-01 00:38:55 +0300

    Finished episode_11

--- app/src/composables/useWorkspace.js
+++ app/src/composables/useWorkspace.js
@@ -2,7 +2,9 @@ import { computed } from "vue";
 import { useAnchorWallet } from "solana-wallets-vue";
 import { Connection } from "@solana/web3.js";
 import { AnchorProvider, Program } from "@coral-xyz/anchor";
-import idl from "../../../target/idl/solana_twitter.json";
+import idl from '@/idl/solana_twitter.json'
+
+const clusterUrl = process.env.VUE_APP_CLUSTER_URL
 
 let workspace = null;
 export const useWorkspace = () => workspace;
@@ -12,8 +14,9 @@ const commitment = 'confirmed'
 
 export const initWorkspace = () => {
   const wallet = useAnchorWallet();
-  const connection = new Connection("http://127.0.0.1:8899", commitment);
-  const provider = computed(() => new AnchorProvider(connection, wallet.value, { preflightCommitment, commitment }));
+  console.log(clusterUrl);
+  const connection = new Connection(clusterUrl, commitment);
+  const provider = computed(() => new AnchorProvider(connection, wallet.value, { preflightCommitment }));
   const program = computed(() => new Program(idl, provider.value));
 
   
